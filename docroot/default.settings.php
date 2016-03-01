6b7dc44a730343b3755ba176d9e577ac9d9643da
sirkitree <sirkitree@gmail.com>
2016-02-29 20:27:54 -0700

    updating default.settings to reflect new method

--- docroot/default.settings.php
+++ docroot/default.settings.php
@@ -34,8 +34,8 @@ $database_prefix = '';
  * $config_directories['staging'] = '/home/myusername/config/staging';
  * @endcode
  */
-$config_directories['active'] = 'files/config_' . md5($database) . '/active';
-$config_directories['staging'] = 'files/config_' . md5($database) . '/staging';
+$config_directories['active'] = '[your-repo-root]/config/kittens';
+$config_directories['staging'] = '[your-repo-root]/config/puppies';
 
 /**
  * Access control for update.php script.
