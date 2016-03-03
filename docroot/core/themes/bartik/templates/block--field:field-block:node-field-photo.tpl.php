39a6fb8ee9bd37e5b0ed94096b7b116137fbf3c8
sirkitree <sirkitree@gmail.com>
2016-03-02 20:37:07 -0700

    adding tpl for our specific layout field block

---
+++ docroot/core/themes/bartik/templates/block--field:field-block:node-field-photo.tpl.php
@@ -0,0 +1,11 @@
+
+<script src="https://aframe.io/releases/0.1.0/aframe.min.js"></script>
+<a-scene>
+
+  <a-camera
+    position="0 1.6 0"></a-camera>
+  <a-sky
+    src="<?php print file_create_url($content['#items'][0]['uri']); ?>"
+    radius="1000"></a-sky>
+
+</a-scene>
