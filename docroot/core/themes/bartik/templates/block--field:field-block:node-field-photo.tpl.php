89cd3fc303f581d034345009fa83a46a0c093933
sirkitree <sirkitree@gmail.com>
2016-03-08 14:36:56 -0700

    rotate the skybox by 270 so it's facing the middle automatically

--- docroot/core/themes/bartik/templates/block--field:field-block:node-field-photo.tpl.php
+++ docroot/core/themes/bartik/templates/block--field:field-block:node-field-photo.tpl.php
@@ -6,6 +6,6 @@
     position="0 1.6 0"></a-camera>
   <a-sky
     src="<?php print file_create_url($content['#items'][0]['uri']); ?>"
-    radius="1000"></a-sky>
+    rotation="0 270 0"></a-sky>
 
 </a-scene>
